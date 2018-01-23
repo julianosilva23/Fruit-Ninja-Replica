3331fab26538e31343f0152febd4aa79fc773d9c
Juliano Silva <julianoluizsilva00@gmail.com>
2018-01-23 19:51:44 -0200

    primeiro commit

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour {
+	
+	public float startForce = 15f;
+
+	Rigidbody2D rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.tag == "Blade")
+		{
+			//Vector3 direction = (col.transform.position - transform.position).normalized;
+            
+            while(gameObject.transform.localScale.x > 0)
+            {
+                gameObject.transform.localScale -= new Vector3(0.001f, 0.001f, 0.001f);
+                Debug.Log(transform.localScale.x);
+            }
+            
+
+
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            Destroy(gameObject, 1.0f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Rede.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rede : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log("rede");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "bom")
+        {
+            Debug.Log("bom");
+            Point(true);
+        }
+        if (col.tag == "ruim")
+        {
+            Debug.Log("ruim");
+            Point(false);
+        }
+    }
+
+    void Point(bool value)
+    {
+
+    }
+
+}
